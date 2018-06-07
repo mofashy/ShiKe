ff44b977caea9cf646485961e817c1103db22258
mofashy <mofashy@163.com>
2018-06-07 21:36:28 +0800

    Initial Commit

---
+++ ShiKe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShiKe
+//
+//  Created by 沈永聪 on 2018/6/7.
+//  Copyright © 2018年 沈永聪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
